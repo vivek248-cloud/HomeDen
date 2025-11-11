81d577e8da604a17155cecda5484e8118e0a5e40
Vivek248-cloud <vivekofficial248.com>
2025-11-11 11:29:53 +0530

    new updates

--- interior/settings.py
+++ interior/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # ✅ Force www redirect
