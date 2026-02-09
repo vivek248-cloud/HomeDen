f6d9b0c9d40907b6111294efa93f32ccccb2fba2
Vivek248-cloud <vivekofficial248.com>
2026-02-09 15:46:01 +0530

    Remove pycache files and add gitignore

--- interior/settings.py
+++ interior/settings.py
@@ -26,15 +26,17 @@ DEBUG = True
 
 
 
-
-# ALLOWED_HOSTS = [
-# '*',
-# ]
+# ALLOWED_HOSTS = ['homedeninterior.com', 'www.homedeninterior.com', 'srv888437.hstgr.cloud', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+'*',
+]
 
 ALLOWED_HOSTS = [
     "homedeninterior.com",
     "www.homedeninterior.com",
     "srv888437.hstgr.cloud",
+    "127.0.0.1",
+    "localhost",
 ]
 
 
