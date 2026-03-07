e41e7c23b1a946e6b585dc0aa64007a36f0dafaa
Vivek248-cloud <vivekofficial248.com>
2026-03-07 16:26:46 +0530

    new feature

--- interior/settings.py
+++ interior/settings.py
@@ -21,8 +21,8 @@ TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 DEBUG = False
 
 # ✅ Force www redirect
-SECURE_SSL_REDIRECT = True
-PREPEND_WWW = True
+# SECURE_SSL_REDIRECT = True
+# PREPEND_WWW = True
 
 
 
